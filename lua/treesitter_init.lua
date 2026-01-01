58db1c0ea393fe2918e9a31302acefa887e6328d
yys <vincnet9th>
2026-01-01 11:29:48 +0000

    update

--- lua/treesitter_init.lua
+++ lua/treesitter_init.lua
@@ -1,7 +1,7 @@
-require'nvim-treesitter'.install { 'rust', 'javascript', 'zig' }
+require'nvim-treesitter'.install { 'c', 'cpp', 'python', 'javascript', 'lua', 'rust', 'zig' }
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'c', 'cpp', 'python', 'javascript', 'lua' },
+  pattern = { 'c', 'cpp', 'python', 'javascript', 'lua', 'rust', 'zig' },
   callback = function() vim.treesitter.start() end,
 })
 
